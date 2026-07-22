6710927f7221a1539074bef73a435dee7f715d15
Ion Lipsiuc <306178285+ionlips@users.noreply.github.com>
2026-07-22 10:28:34 +0100

    fix(nvim): point Markdown's linter to rules
    Neovim uses nvim-lint for linting, which makes use of markdownlint
    (through the markdownlint-cli2 CLI). Without this change, the linter
    will look for .markdownlint.yaml (or .markdownlint.jsonc) in the current
    working directory. Thus, one must explicitly point towards the file so
    that it works regardless of the current working directory.

---
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,14 @@
+return {
+    "mfussenegger/nvim-lint",
+    opts = {
+        linters = {
+            ["markdownlint-cli2"] = {
+                args = {
+                    "--config",
+                    vim.fn.expand("~/.markdownlint.yaml"),
+                    "-",
+                },
+            },
+        },
+    },
+}
