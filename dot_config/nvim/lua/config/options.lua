55797ccf96c040ce9e97f004eec385d68ee17bc4
Ion Lipsiuc <306178285+ionlips@users.noreply.github.com>
2026-09-07 13:49:57 +0100

    fix(nvim): bridge Neovim and tmux via OSC 52

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,4 +1,17 @@
-if vim.env.TMUX then
+if vim.env.SSH_TTY then
+    vim.g.clipboard = {
+        name = "OSC 52",
+        copy = {
+            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+        },
+        paste = {
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+        },
+    }
+    vim.opt.clipboard = "unnamedplus"
+elseif vim.env.TMUX then
     vim.g.clipboard = {
         name = "tmux",
         copy = {
