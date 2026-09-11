41634b742c1efe51f1c1fd4b1a11c866b9c5159c
Ion Lipsiuc <306178285+ionlips@users.noreply.github.com>
2026-09-11 10:35:59 +0100

    feat: prefer LazyVim's default dashboard

--- dot_config/nvim/lua/plugins/snacks.nvim.lua
+++ dot_config/nvim/lua/plugins/snacks.nvim.lua
@@ -1,7 +1,6 @@
 return {
     "folke/snacks.nvim",
     opts = {
-        dashboard = { enabled = false }, -- Prefer dashboard-nvim over this.
         picker = {
             hidden = true,
             ignored = true,
